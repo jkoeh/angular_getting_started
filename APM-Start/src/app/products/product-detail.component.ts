f3cd071d57454915a7d33a16107d41e756ec0205
jkoeh <johnkoeh@gmail.com>
2020-09-04 17:29:41 -0700

    route param

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,15 +11,23 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string;
   product: IProduct;
   errorMessage: string;
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.productService.getProduct(1).subscribe({
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.productService.getProduct(id).subscribe({
       next: (x) => {
         this.product = x;
         this.pageTitle = x.productName;
       },
       error: (err) => (this.errorMessage = err),
     });
+  }
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 }
